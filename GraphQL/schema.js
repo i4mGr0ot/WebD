b612b466db52d2256068514251931c87d7f1dc49
I am Groot <144456703+i4mGr0ot@users.noreply.github.com>
2024-07-09 22:32:57 +0530

    Update schema.js

--- GraphQL/schema.js
+++ GraphQL/schema.js
@@ -3,20 +3,40 @@ export const typeDefs = `#graphql
     id: ID!
     title: String!
     platform: [String!]!
+    reviews: [Review!]
   }
   type Review {
     id: ID!
     rating: Int!
     content: String!
+    author: Author!
+    game: Game!
   }
   type Author {
     id: ID!
     name: String!
     verified: Boolean!
+    reviews: [Review!]
   }
   type Query {
     games: [Game]
+    game(id: ID!): Game
     reviews: [Review]
+    review(id: ID!): Review
     authors: [Author]
+    author(id: ID!): Author
+  }
+  type Mutation {
+    addGame(game: AddGameInput!): Game
+    deleteGame(id: ID!): [Game]
+    updateGame(id: ID!, edits: EditGameInput): Game
+  }
+  input AddGameInput {
+    title: String!,
+    platform: [String!]!
+  }
+  input EditGameInput {
+    title: String,
+    platform: [String!]
   }
 `
